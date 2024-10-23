6b93dd5231c47e5ed4f37dc7c85174d043a6d48d
Planetoid555 <snaxshop1@gmail.com>
2024-10-23 16:20:32 -0400

    Update PlayerHealth.cs
    Added a health system, will update more

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
+    public int maxHP;
+    private int currentHP;
+    void Start() {
+        currentHP = maxHP;
+    }
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Enemy") {
             Debug.Log("worm hurt");
-            // take damage stuff 
-            Destroy(gameObject, 0.1f);
+            currentHP -= 1;
+            if (currentHP < 0) {
+                Destroy(gameObject, 0.1f);
+            }
         }
     }
 }
